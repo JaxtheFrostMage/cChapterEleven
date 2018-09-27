c21877467d981214f7493bf2fd47e29dc4b5e339
Evan Raker <er306632@students.davenport.k12.ia.us>
2018-09-27 11:17:09 -0500

    yes

---
+++ XYZ Company/XYZ Company/Source.cpp
@@ -0,0 +1,22 @@
+//XYZ Comapny. cpp
+//Author: Evan J. Raker
+//Date: 9/27/2018
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	
+	int valuesArray[7] = { 50, 10, 5, 25, 30, 15, 20};
+	int total = 0;
+	for (int i = 0; i < size(valuesArray); i++){
+
+		cout << valuesArray[i]<< endl;
+		total = total + valuesArray[i];
+	}
+	cout << total;
+
+	system("pause");
+	return 0;
+}
