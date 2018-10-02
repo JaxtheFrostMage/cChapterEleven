a3c87cbf3cd18259dec050b47a37af86e68ebdeb
Evan Raker <er306632@students.davenport.k12.ia.us>
2018-10-02 10:02:32 -0500

    fixed numbering

--- Min, Max, and Average/Min, Max, and Average/Source.cpp
+++ Min, Max, and Average/Min, Max, and Average/Source.cpp
@@ -15,7 +15,7 @@ int main(){
 	double scoreArray[10] = { 0.0 };
 
 	for (int i = 0; i < size(scoreArray); i++) {
-		cout << "Enter test score "<< scoreArray[i] + 1 <<": ";
+		cout << "Enter test score "<< i + 1 <<": ";
 		cin >> scoreArray[i];
 
 		if (scoreArray[i] >= 0 && scoreArray[i] <= 100) {
