d4c1e095dc7e81ee86aa223fc1ce50ccffae69fa
Evan Raker <er306632@students.davenport.k12.ia.us>
2018-10-04 10:02:24 -0500

    working

--- Advanced 28/Advanced 28/Source.cpp
+++ Advanced 28/Advanced 28/Source.cpp
@@ -18,26 +18,29 @@ int main()
 	int total = 0;
 	int scores[20] = { 0 };
 	total = 0;
-
+	srand(time(NULL));
 	
 	
 
 	for (int i = 0; i < size(scores); i++) {
-		srand(time(NULL));
 		scores[i] = rand() % 100 + 1;
-		if (scores[i] == searchScore)
-			total += 1;
 	}
+
 	for (int i = 0; i < size(scores); i++) {
 		cout << scores[i] << endl;
 	}
-	cout << "Enter a score from 0 through 100 (-1 to end): ";
-	cin >> searchScore;
 
 
-	while (searchScore >= 0)
+
+	do
 	{
-	
+
+		cout << "Enter a score from 0 through 100 (-1 to end): ";
+		cin >> searchScore;
+		
+		for(int i = 0; i < size(scores); i++)
+		if (scores[i] == searchScore)
+			total += 1;
 		//search for score
 		
 		//end if    
@@ -47,11 +50,9 @@ int main()
 		cout << "Number of students earning a score of "
 			<< searchScore << ": "
 			<< total << endl << endl;
+		total = 0;
+	} while (searchScore >= 0);  //end while
 
-		cout << "Enter a score from 0 through 100 (-1 to end): ";
-		cin >> searchScore;
-	}  //end while
-
-	   //system("pause");
+	system("pause");
 	return 0;
 }	//end of main function
